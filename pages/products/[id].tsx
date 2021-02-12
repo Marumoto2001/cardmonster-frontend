6a1040698b4c7de918f0ab97bd453a893c649943
Noél Sigmunczyk <linlogge@users.noreply.github.com>
2021-02-13 00:27:08 +0100

    add basic product search

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,7 +1,22 @@
 import { useRouter } from 'next/router'
+import { products } from "./index"
 
 export default function Product() {
     const router = useRouter()
     const { id } = router.query
-    return <h1>Produkt: {id}</h1>       
+
+    let product = products.find(el => id == el.id)
+
+    return (
+        <>
+            { product != undefined ? (
+                <>
+                    <h1>Produkt: {product.title}</h1>
+                    <p>Price: {product.price}</p>
+                </>
+            ): (
+                <h1>Das Produkt konnte nicht gefunden werden.</h1>
+            )}
+        </>
+    )   
 }
--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,3 +1,38 @@
-export default function Products(){
-    return <h1>Produktübersicht</h1>
+import Link from "next/link"
+
+export interface Product {
+    id: string;
+    title: string;
+    price: number;
+}
+
+export const products: Product[] = [
+    {
+        id: "1",
+        title: "Glurak",
+        price: 700,
+    },
+    {
+        id: "2",
+        title: "Pikachu",
+        price: 200,
+    },
+    {
+        id: "3",
+        title: "Glumanda",
+        price: 200,
+    }
+]
+
+export default function Products() {
+    return (
+        <>
+        <h1>Produktübersicht</h1>
+        <ul>
+            { products.map(product => (
+                <li>{product.title}, Preis: {product.price}€, <Link href={`/products/${product.id}`}>Zum Produkt</Link></li>
+            ))}
+        </ul>
+        </>
+    )
 }
