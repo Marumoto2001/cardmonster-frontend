86cfa549312fabd1202b1e4eb8bf32de4737bc4f
Marcus Hoppe <Marumoto@web.de>
2021-02-16 11:01:40 +0000

    bug fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,12 @@
 export default function Home() {
   return (
     <h1>Startseite</h1>
+    //<h2>Wilkommen auf Cardmonster hier werden ihere Kartenwünsche wahr!!</h2>
   )
 }
+/*export default function Home2() {
+  return (
+    //<h1>Startseite</h1>
+    <h2>Wilkommen auf Cardmonster hier werden ihere Kartenwünsche wahr!!</h2>
+  )
+}*/
