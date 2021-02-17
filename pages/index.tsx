76249be800f052520e5b823a0d7fbf78cc410d41
Marcus Hoppe <Marumoto@web.de>
2021-02-17 13:56:05 +0000

    Startpage changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 export default function Home() {
   return (
       <>
-        <h1>Startseite</h1>
-        <h2>Hall</h2>
+        <h1>Willkommen auf Cardmonster.de</h1>
+        <h2>Hier werden ihre Kartenwünsche wahr!!</h2>
+        <h3>Hier wird mit Highspeed an ihren Bestellungen gearbeitet^^</h3>
+        <h4>Unsere Pokemon arbeiten 24/7 an iheren Bestellungen 
+            und Ash haben wir die Monsterguten Fangreise zu verdanken</h4>
         </>
     )
 }
